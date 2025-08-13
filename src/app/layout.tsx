50d03c494a9e7a46e7ef625cfc1f5b9de7d179b9
cforChobar <sidhuashman02@gmail.com>
2025-08-13 09:48:47 +0530

    project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playfair_Display, Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const mainFont = Playfair_Display({
+  variable: "--font-playfair-serif",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const bodyFont = Lato({
+  variable: "--font-lato-sans",
+  weight : ["400", "700"],
   subsets: ["latin"],
 });
 
@@ -24,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${mainFont.variable} ${bodyFont.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
